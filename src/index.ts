ca1215497b988c71cfe0511a14002d018d7256cb
Nicolas Picotto <nicopicotto@hotmail.com>
2024-12-03 20:02:36 -0300

    saque error middleware

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { connectDB } from "./config/mongo";
 import express from "express";
 import budgetRouter from "./routes/budgetRouter";
 import cors from "cors";
-import errorMiddleware from "./middleware/errorMiddleware";
+
 import { checkJWT } from "./middleware/envMiddleware";
 import userRouter from "./routes/userRouter";
 
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(errorMiddleware);
 
 app.use(checkJWT);
 
